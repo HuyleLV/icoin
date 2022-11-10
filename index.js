5a2c1aa1c18e688133a7c81cd198727291a17428
HuyleLV <88972121+HuyleLV@users.noreply.github.com>
2022-11-10 00:23:39 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+const bodyparser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config({path: './.env'});
+
+const port = 5000;
+
+var cors = require('cors')
+app.use(cors());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.listen(port,()=>console.log(`App listening at http://localhost:${port}`))
